0de9be9ae8b38d73e69def1b81fb7a88de3df6ba
Shambhuk <shambhuk@chetu.com>
2017-08-11 16:05:30 +0530

    [Shambhu]----Chagnges added comments

--- Desktop/my_first_repo-master/my_first_repo-master/dummy.cpp
+++ Desktop/my_first_repo-master/my_first_repo-master/dummy.cpp
@@ -1,1 +1,1 @@
-char choice;cout<<"HELLO THIS IS A DUMMY CODE";cin>>choice;if(choice=='a'||choice=='e'||choice=='i'||choice=='o'||choice=='u')cout<<"vowel";elsecout<<"consonant";
+char choice;//Shambhucout<<"HELLO THIS IS A DUMMY CODE";cin>>choice;if(choice=='a'||choice=='e'||choice=='i'||choice=='o'||choice=='u')cout<<"vowel";elsecout<<"consonant";
